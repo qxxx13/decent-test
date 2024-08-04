da8b37379e305c186f6559e56e7fd39928e38fb1
Nikolay Nazarenko <redecv9@gmail.com>
2024-08-04 23:43:41 +0300

    fix: eslint

--- src/Pages/CountryPage/CountryPage.tsx
+++ src/Pages/CountryPage/CountryPage.tsx
@@ -3,10 +3,10 @@ import { useUnit } from 'effector-react';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
+import { ErrorHandler } from '../../components/ErrorHandler/ErrorHandler';
 import { CountryType } from '../../types/CountryType';
 import { CountryDescCard } from './CountryDescCard/CountryDescCard';
 import { $countryStoreGetStatus, clearCountryStore, fetchCountryByNameFx } from './model/countryStore';
-import { ErrorHandler } from '../../components/ErrorHandler/ErrorHandler';
 
 export const CountryPage = () => {
     const countryName = (useParams().country as string).replaceAll('_', ' ');
